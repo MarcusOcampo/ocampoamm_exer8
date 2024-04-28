360efe44385b535b3779d6e190eee8057f15b124
MarcusOcampo <amocampo9@up.edu.ph>
2024-04-28 09:19:19 +0800

    test push

---
+++ README.txt
@@ -0,0 +1,13 @@
+ReadMe:
+
+Full Name: Alessandro Marcus M. Ocampo
+Section: U5L
+Code Description: 
+
+
+My notes:
+1. open ubuntu terminal
+2. cd /mnt/c/Users/Public/ocampo_exer8/ocampoamm_exer8/react-first
+Additional. npm install -g nodemon
+3. nodemon npm start
+
---
+++ react-first/src/App.js
@@ -0,0 +1,27 @@
+import SubjectList from './subjectList'
+
+function App() {
+  return (
+    <div>
+        <div>
+        Hello World!
+        <SubjectList data={CMSCSubjects} title={'Computer Science'} />
+        <SubjectList data={CHEMSubjects} title={'Chemistry'} />
+      </div>
+    </div>
+  );
+}
+
+const CMSCSubjects = [
+  { code: "CMSC100", description: "Web Programming", id: 1 },
+  { code: "CMSC150", description: "Scientific Computation", id: 2 },
+  { code: "CMSC22", description: "Object-Oriented Programming", id: 3 },
+];
+
+const CHEMSubjects = [
+  { code: "CHEM100", description: "Introduction to Organic Chemistry", id: 1 },
+  { code: "CHEM10", description: "Chemical Kinetics", id: 2 },
+  { code: "CHEM11", description: "Introduction to Biochemistry", id: 3 },
+];
+
+export default App;
